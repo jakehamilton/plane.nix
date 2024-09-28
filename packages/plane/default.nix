148ef0d20410b350f58dd7a062dc8da1cc3c91ec
Jake Hamilton <jake.hamilton@hey.com>
2024-09-27 21:35:34 -0700

    feat: working admin build

--- packages/plane/default.nix
+++ packages/plane/default.nix
@@ -8,8 +8,12 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "makeplane";
     repo = "plane";
-    rev = "0068ea93deeef5ef2f52f6116483a42c738bce06";
-    sha256 = "06pfjzzymf0kz1zbpqz6ql137bp0yizfhx34kspvljqrd9v362z5";
+		rev = "707570ca7ab95d6680953de939f6acf78480ae01";
+		sha256 = "09kn6y8s6ziqmpn8x4cfsis3q5a9ii6ckvm3b71l73w7j78f70zq";
+
+		# TODO: Upgrade to latest when `web` no longer fails to build.
+    # rev = "0068ea93deeef5ef2f52f6116483a42c738bce06";
+    # sha256 = "06pfjzzymf0kz1zbpqz6ql137bp0yizfhx34kspvljqrd9v362z5";
   };
 
 	includeNodeModulesPath = ''
@@ -24,6 +28,12 @@ let
 		ln -s $typescript_config_pkg ./node_modules/${workspace.${name}.pname}
 	'';
 
+	# NOTE: The build system for some packages seems to choke when trying to stat this recursive link.
+	# Thankfully we don't need it and can safely get rid of it for these cases.
+	removeRecursiveSelfLink = ''
+		rm -r deps/$pname/$(basename $pname)
+	'';
+
 	yarnBuild = ''
 		pushd deps/$pname
 
@@ -32,18 +42,35 @@ let
 		popd
 	'';
 
+	# NOTE: We have to use `--experimental-build-mode compile` here due to an error with builds failing otherwise.
+	# Without it, there is an issue with `useContext` not finding the appropriate context for `usePathname`.
+	# Potentially relevant issue:
+	# https://github.com/vercel/next.js/issues/63123
+	nextBuild = ''
+		pushd deps/$pname
+
+		./node_modules/.bin/next build --experimental-build-mode compile
+
+		popd
+	'';
+
+	addServerBin = ''
+		makeWrapper ${pkgs.nodejs}/bin/node $out/bin/server \
+			--add-flags "$out/libexec/$pname/deps/$pname/.next/standalone/server.js"
+	'';
+
   workspace = mkYarnWorkspace {
     inherit src;
     packageOverrides = {
-      plane-typescript-config = {};
+			# TODO: Submit a PR to make this a proper dependency of the other packages.
+			# Packages don't properly add this package to their dev dependencies so we have to link
+			# it manually instead.
+			# ${withLinkedModule "plane-typescript-config"}
+      # plane-typescript-config = {};
 
       plane-helpers = {
         buildPhase = ''
 					${includeNodeModulesPath}
-
-					# Packages don't properly add this package to their dev dependencies so we have to link
-					# it manually instead.
-					${withLinkedModule "plane-typescript-config"}
 
 					${yarnBuild}
 				'';
@@ -53,13 +80,11 @@ let
         buildPhase = ''
 					${includeNodeModulesPath}
 
-					# Packages don't properly add this package to their dev dependencies so we have to link
-					# it manually instead.
-					${withLinkedModule "plane-typescript-config"}
-
 					${yarnBuild}
 				'';
 
+				# TODO: Submit a PR to add "src/**" to the files array in package.json so that the packed
+				# tarball includes the source files which other packages try to link to for types.
 				postInstall = ''
 					pushd $out/libexec/$pname/deps/$pname
 
@@ -74,11 +99,29 @@ let
         buildPhase = ''
 					${includeNodeModulesPath}
 
-					# Packages don't properly add this package to their dev dependencies so we have to link
-					# it manually instead.
-					${withLinkedModule "plane-typescript-config"}
-
 					${yarnBuild}
+				'';
+      };
+
+      admin = {
+				doDist = false;
+
+				nativeBuildInputs = [
+					pkgs.makeWrapper
+				];
+
+        buildPhase = ''
+					${includeNodeModulesPath}
+
+					pushd deps/admin
+						rm -rf admin
+					popd
+
+					${nextBuild}
+				'';
+
+				postInstall = ''
+					${addServerBin}
 				'';
       };
 
@@ -86,22 +129,15 @@ let
         buildPhase = ''
 					${includeNodeModulesPath}
 
-					# Packages don't properly add this package to their dev dependencies so we have to link
-					# it manually instead.
-					${withLinkedModule "plane-typescript-config"}
-
-					ls -la $(realpath node_modules/@plane/ui)
-					exit 1
-
 					pushd deps/web
 						rm -rf web
 					popd
 
-					${yarnBuild}
+					${nextBuild}
 				'';
       };
     };
   };
 in
 	(builtins.trace (builtins.attrNames workspace))
-workspace.web
+workspace.admin
